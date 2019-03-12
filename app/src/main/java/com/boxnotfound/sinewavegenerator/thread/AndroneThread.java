ba84bde5e6c60a283eed1be620b303a92bd2a48a
Kevin Mark Price <KPrice725@aol.com>
2019-03-12 15:00:20 -0400

    Add AudioTrack instantiations

--- app/src/main/java/com/boxnotfound/sinewavegenerator/thread/AndroneThread.java
+++ app/src/main/java/com/boxnotfound/sinewavegenerator/thread/AndroneThread.java
@@ -1,5 +1,6 @@
 package com.boxnotfound.sinewavegenerator.thread;
 
+import android.media.AudioAttributes;
 import android.media.AudioFormat;
 import android.media.AudioTrack;
 
@@ -15,8 +16,26 @@ public class AndroneThread extends Thread {
     @Override
     public void run() {
         super.run();
-        soundOn = false;
+        soundOn = true;
         int bufferSize = AudioTrack.getMinBufferSize(sampleRate, AudioFormat.CHANNEL_OUT_MONO, AudioFormat.ENCODING_PCM_16BIT);
+
+        AudioAttributes audioAttributes = new AudioAttributes.Builder()
+                .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+                .setUsage(AudioAttributes.USAGE_MEDIA)
+                .build();
+
+        AudioFormat audioFormat = new AudioFormat.Builder()
+                .setSampleRate(sampleRate)
+                .setEncoding(AudioFormat.ENCODING_PCM_16BIT)
+                .setChannelMask(AudioFormat.CHANNEL_OUT_MONO)
+                .build();
+
+        AudioTrack track = new AudioTrack.Builder()
+                .setAudioAttributes(audioAttributes)
+                .setAudioFormat(audioFormat)
+                .setBufferSizeInBytes(bufferSize)
+                .build();
+
 
     }
 
