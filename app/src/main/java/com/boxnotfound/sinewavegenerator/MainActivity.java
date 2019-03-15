e7c27db3678fcee7e207e69aaf7d76be71331623
Kevin Mark Price <KPrice725@aol.com>
2019-03-15 15:31:23 -0400

    Lay groundwork for implementing Androne RecyclerView

--- app/src/main/java/com/boxnotfound/sinewavegenerator/MainActivity.java
+++ app/src/main/java/com/boxnotfound/sinewavegenerator/MainActivity.java
@@ -7,18 +7,23 @@ import android.media.AudioTrack;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 
 import com.boxnotfound.sinewavegenerator.constants.WaveForm;
 import com.boxnotfound.sinewavegenerator.instance.Androne;
+import com.boxnotfound.sinewavegenerator.ui.AndroneViewAdapter;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String LOG_TAG = MainActivity.class.getSimpleName();
     private static final double defaultFrequency = 440.0;
     private Androne sineAndrone, squareAndrone, triangleAndrone, sawtoothAndrone;
+    private RecyclerView androneView;
+    private AndroneViewAdapter androneViewAdapter;
+    private RecyclerView.LayoutManager androneViewManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
---
+++ app/src/main/java/com/boxnotfound/sinewavegenerator/ui/AndroneViewAdapter.java
@@ -0,0 +1,10 @@
+package com.boxnotfound.sinewavegenerator.ui;
+
+import android.support.v7.widget.RecyclerView;
+
+public class AndroneViewAdapter extends RecyclerView.Adapter {
+
+    public static class AndroneViewHolder extends RecyclerView.ViewHolder {
+
+    }
+}
