c5fbac0b2828e66437875d1a0e43318449cba9bc
Kevin Mark Price <KPrice725@aol.com>
2019-03-17 17:43:35 -0400

    Build RecyclerView's ViewHolder constructor and member variables

--- app/src/main/java/com/boxnotfound/sinewavegenerator/ui/AndroneViewAdapter.java
+++ app/src/main/java/com/boxnotfound/sinewavegenerator/ui/AndroneViewAdapter.java
@@ -1,10 +1,40 @@
 package com.boxnotfound.sinewavegenerator.ui;
 
+import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.SeekBar;
+import android.widget.Spinner;
+import android.widget.TextView;
 
-public class AndroneViewAdapter extends RecyclerView.Adapter {
+import com.boxnotfound.sinewavegenerator.R;
 
-    public static class AndroneViewHolder extends RecyclerView.ViewHolder {
+public class AndroneViewAdapter extends RecyclerView.Adapter<AndroneViewAdapter.ViewHolder> {
 
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        public EditText androneName, androneFrequency;
+        public TextView androneCents;
+        public Spinner androneWaveform, andronePitch;
+        public SeekBar androneVolume;
+        public ImageButton androneFrequencyIncrement, androneFrequencyDecrement, andronePitchIncrement, andronePitchDecrement, andronePlay;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            androneName = itemView.findViewById(R.id.androne_name_edittext);
+            androneFrequency = itemView.findViewById(R.id.frequency_edittext);
+            androneCents = itemView.findViewById(R.id.cents_textview);
+            androneWaveform = itemView.findViewById(R.id.waveform_spinner);
+            andronePitch = itemView.findViewById(R.id.pitch_spinner);
+            androneVolume = itemView.findViewById(R.id.volume_seekbar);
+            androneFrequencyIncrement = itemView.findViewById(R.id.frequency_hz_increment);
+            androneFrequencyDecrement = itemView.findViewById(R.id.frequency_hz_decrement);
+            andronePitchIncrement = itemView.findViewById(R.id.pitch_increment);
+            andronePitchDecrement = itemView.findViewById(R.id.pitch_decrement);
+            andronePlay = itemView.findViewById(R.id.play_toggle_button);
+        }
     }
 }
