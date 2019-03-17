d29a1fde4b868ecdc12637a10fdedbc0c43c5383
Kevin Mark Price <KPrice725@aol.com>
2019-03-17 17:49:07 -0400

    Build out RecyclerView onCreateViewHolder and getItemCount methods

--- app/src/main/java/com/boxnotfound/sinewavegenerator/ui/AndroneViewAdapter.java
+++ app/src/main/java/com/boxnotfound/sinewavegenerator/ui/AndroneViewAdapter.java
@@ -1,8 +1,11 @@
 package com.boxnotfound.sinewavegenerator.ui;
 
+import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.SeekBar;
@@ -10,8 +13,39 @@ import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.boxnotfound.sinewavegenerator.R;
+import com.boxnotfound.sinewavegenerator.instance.Androne;
+
+import java.util.ArrayList;
 
 public class AndroneViewAdapter extends RecyclerView.Adapter<AndroneViewAdapter.ViewHolder> {
+
+    private ArrayList<Androne> androneList;
+
+    public AndroneViewAdapter(ArrayList<Androne> androneList) {
+        this.androneList = androneList;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+
+        View androneView = inflater.inflate(R.layout.androne_rv_item, parent, false);
+
+        ViewHolder viewHolder = new ViewHolder(androneView);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int position) {
+        Androne androne = androneList.get(position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return androneList.size();
+    }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
 
@@ -37,4 +71,6 @@ public class AndroneViewAdapter extends RecyclerView.Adapter<AndroneViewAdapter.
             andronePlay = itemView.findViewById(R.id.play_toggle_button);
         }
     }
+
+
 }
