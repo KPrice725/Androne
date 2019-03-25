f51f9580e744b70a51aa2e2fceabc1e71b04b6a2
Kevin Mark Price <KPrice725@aol.com>
2019-03-25 19:28:21 -0400

    Add base functions

--- app/src/main/java/com/boxnotfound/sinewavegenerator/BasePresenter.java
+++ app/src/main/java/com/boxnotfound/sinewavegenerator/BasePresenter.java
@@ -1,4 +1,6 @@
 package com.boxnotfound.sinewavegenerator;
 
 public interface BasePresenter {
+    void onStart();
+    void onDestroy();
 }
--- app/src/main/java/com/boxnotfound/sinewavegenerator/BaseView.java
+++ app/src/main/java/com/boxnotfound/sinewavegenerator/BaseView.java
@@ -1,4 +1,5 @@
 package com.boxnotfound.sinewavegenerator;
 
-public interface BaseView {
+public interface BaseView<T> {
+    void setPresenter(T presenter);
 }
