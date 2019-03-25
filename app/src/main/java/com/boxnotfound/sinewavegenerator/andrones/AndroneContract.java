1c880cda54a1ccb157d5140cc63fbd0b6f7a8340
Kevin Mark Price <KPrice725@aol.com>
2019-03-25 19:38:35 -0400

    Initial Commit

---
+++ app/src/main/java/com/boxnotfound/sinewavegenerator/andrones/AndroneContract.java
@@ -0,0 +1,15 @@
+package com.boxnotfound.sinewavegenerator.andrones;
+
+import com.boxnotfound.sinewavegenerator.BasePresenter;
+import com.boxnotfound.sinewavegenerator.BaseView;
+
+public interface AndroneContract {
+
+    interface Presenter implements BasePresenter {
+
+    }
+
+    interface View implements BaseView<Presenter> {
+
+    }
+}
