636250267324c008637723d8c52ce59360effa41
Kevin Mark Price <KPrice725@aol.com>
2019-03-27 09:35:27 -0400

    Add Dao implementations for Androne and Pitch

--- app/src/main/java/com/boxnotfound/sinewavegenerator/model/AndroneDao.java
+++ app/src/main/java/com/boxnotfound/sinewavegenerator/model/AndroneDao.java
@@ -1,4 +1,30 @@
 package com.boxnotfound.sinewavegenerator.model;
 
+import android.arch.lifecycle.LiveData;
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.OnConflictStrategy;
+import android.arch.persistence.room.Query;
+import android.arch.persistence.room.Update;
+
+import java.util.List;
+
+@Dao
 public interface AndroneDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insert(Androne androne);
+
+    @Query("SELECT * FROM androne_table ORDER BY id ASC")
+    LiveData<List<Androne>> getAllAndrones();
+
+    @Update
+    void updateAndrone(Androne androne);
+
+    @Delete
+    void deleteAndrone(Androne androne);
+
+    @Query("DELETE FROM androne_table")
+    void deleteAllAndrones();
 }
--- app/src/main/java/com/boxnotfound/sinewavegenerator/model/PitchDao.java
+++ app/src/main/java/com/boxnotfound/sinewavegenerator/model/PitchDao.java
@@ -1,4 +1,25 @@
 package com.boxnotfound.sinewavegenerator.model;
 
+import android.arch.lifecycle.LiveData;
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.OnConflictStrategy;
+import android.arch.persistence.room.Query;
+
+import java.util.List;
+
+@Dao
 public interface PitchDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insert(Pitch pitch);
+
+    @Query("SELECT * FROM pitch_table ORDER BY id ASC")
+    LiveData<List<Pitch>> getAllPitches();
+
+    @Query("SELECT * FROM pitch_table WHERE pitch_name = :pitchName")
+    LiveData<Pitch> getPitch(String pitchName);
+
+    @Query("DELETE FROM pitch_table")
+    void deleteAllPitches();
 }
