700902e4ed5109d8122ca9dacf53caf266fbe61a
Kevin Mark Price <KPrice725@aol.com>
2019-03-27 11:10:11 -0400

    Implement methods and singleton Instance

--- app/src/main/java/com/boxnotfound/sinewavegenerator/model/room/AppDatabase.java
+++ app/src/main/java/com/boxnotfound/sinewavegenerator/model/room/AppDatabase.java
@@ -1,6 +1,35 @@
 package com.boxnotfound.sinewavegenerator.model.room;
 
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
+import android.content.Context;
 
-public class AppDatabase extends RoomDatabase {
+import com.boxnotfound.sinewavegenerator.model.Androne;
+import com.boxnotfound.sinewavegenerator.model.AndroneDao;
+import com.boxnotfound.sinewavegenerator.model.Pitch;
+import com.boxnotfound.sinewavegenerator.model.PitchDao;
+
+@Database(entities = {Androne.class, Pitch.class}, version = 1)
+public abstract class AppDatabase extends RoomDatabase {
+
+    public abstract AndroneDao androneDao();
+
+    public abstract PitchDao pitchDao();
+
+    private static volatile AppDatabase INSTANCE;
+
+    public static AppDatabase getDatabase(final Context context) {
+        if (INSTANCE == null) {
+            synchronized (AppDatabase.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                            AppDatabase.class, "app_database")
+                            .build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+
 }
