cc0a1a90c4531f151e44949091d2dc9509dd2f16
Kevin Mark Price <KPrice725@aol.com>
2019-03-27 11:39:47 -0400

    Implement dao access methods

--- app/src/main/java/com/boxnotfound/sinewavegenerator/model/AndroneRepository.java
+++ app/src/main/java/com/boxnotfound/sinewavegenerator/model/AndroneRepository.java
@@ -1,4 +1,94 @@
 package com.boxnotfound.sinewavegenerator.model;
 
+import android.app.Application;
+import android.arch.lifecycle.LiveData;
+import android.os.AsyncTask;
+
+import com.boxnotfound.sinewavegenerator.model.room.AppDatabase;
+
+import java.util.List;
+
 public class AndroneRepository {
+
+    private AndroneDao androneDao;
+    private LiveData<List<Androne>> allAndrones;
+
+    public AndroneRepository(Application application) {
+        AppDatabase db = AppDatabase.getDatabase(application);
+        androneDao = db.androneDao();
+        allAndrones = androneDao.getAllAndrones();
+    }
+
+    public LiveData<List<Androne>> getAllAndrones() { return allAndrones; }
+
+
+    public void insert(Androne androne) {
+        new insertAndroneAsyncTask(androneDao).execute(androne);
+    }
+
+    private static class insertAndroneAsyncTask extends AsyncTask<Androne, Void, Void> {
+
+        private AndroneDao asyncAndroneDao;
+
+        insertAndroneAsyncTask(AndroneDao dao) { asyncAndroneDao = dao; }
+
+        @Override
+        protected Void doInBackground(Androne... params) {
+            asyncAndroneDao.insert(params[0]);
+            return null;
+        }
+    }
+
+    public void updateAndrone(final Androne androne) {
+        new updateAndroneAsyncTask(androneDao).execute(androne);
+    }
+
+    private static class updateAndroneAsyncTask extends AsyncTask<Androne, Void, Void> {
+
+        private AndroneDao asyncAndroneDao;
+
+        updateAndroneAsyncTask(AndroneDao dao) { asyncAndroneDao = dao; }
+
+        @Override
+        protected Void doInBackground(Androne... params) {
+            asyncAndroneDao.updateAndrone(params[0]);
+            return null;
+        }
+    }
+
+    public void deleteAndrone(final Androne androne) {
+        new deleteAndroneAsyncTask(androneDao).execute(androne);
+    }
+
+    private static class deleteAndroneAsyncTask extends AsyncTask<Androne, Void, Void> {
+
+        private AndroneDao asyncAndroneDao;
+
+        deleteAndroneAsyncTask(AndroneDao dao) { asyncAndroneDao = dao; }
+
+        @Override
+        protected Void doInBackground(Androne... params) {
+            asyncAndroneDao.deleteAndrone(params[0]);
+            return null;
+        }
+    }
+
+    public void deleteAllAndrones() {
+        new deleteAllAndronesAsyncTask(androneDao).execute();
+    }
+
+    private static class deleteAllAndronesAsyncTask extends AsyncTask<Androne, Void, Void> {
+
+        private AndroneDao asyncAndroneDao;
+
+        deleteAllAndronesAsyncTask(AndroneDao dao) { asyncAndroneDao = dao; }
+
+        @Override
+        protected Void doInBackground(Androne... params) {
+            asyncAndroneDao.deleteAllAndrones();
+            return null;
+        }
+    }
+
+
 }
