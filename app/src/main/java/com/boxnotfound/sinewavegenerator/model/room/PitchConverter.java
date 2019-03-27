82304d416330310fd346d5084e075ab606e9a4b2
Kevin Mark Price <KPrice725@aol.com>
2019-03-27 08:23:32 -0400

    Implement TypeConverter to and from methods

--- app/src/main/java/com/boxnotfound/sinewavegenerator/model/room/PitchConverter.java
+++ app/src/main/java/com/boxnotfound/sinewavegenerator/model/room/PitchConverter.java
@@ -1,4 +1,17 @@
 package com.boxnotfound.sinewavegenerator.model.room;
 
+import android.arch.persistence.room.TypeConverter;
+
+import com.boxnotfound.sinewavegenerator.model.Pitch;
+
 public class PitchConverter {
+    @TypeConverter
+    public Pitch fromFrequency(Double frequency) {
+        return frequency == null ? null : new Pitch(frequency);
+    }
+
+    @TypeConverter
+    public Double pitchToFrequency(Pitch pitch) {
+        return pitch == null ? null : pitch.getFrequency();
+    }
 }
--- app/src/main/java/com/boxnotfound/sinewavegenerator/model/room/WaveformConverter.java
+++ app/src/main/java/com/boxnotfound/sinewavegenerator/model/room/WaveformConverter.java
@@ -1,4 +1,26 @@
 package com.boxnotfound.sinewavegenerator.model.room;
 
+import android.arch.persistence.room.TypeConverter;
+
+import com.boxnotfound.sinewavegenerator.constants.Waveform;
+
 public class WaveformConverter {
+    @TypeConverter
+    public Waveform fromWaveformString(String waveform) {
+        if (waveform == null) {
+            return null;
+        } else {
+            try {
+                return Waveform.valueOf(waveform);
+            } catch (IllegalArgumentException e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+    }
+
+    @TypeConverter
+    public String waveformToString(Waveform waveform) {
+        return waveform == null ? null : waveform.toString();
+    }
 }
