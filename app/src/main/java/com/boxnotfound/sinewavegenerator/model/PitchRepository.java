6c504687b103ffb598a4ac9fc06ed921b02571ed
Kevin Mark Price <KPrice725@aol.com>
2019-03-27 12:05:49 -0400

    Implement PitchDao access methods

--- app/src/main/java/com/boxnotfound/sinewavegenerator/model/PitchRepository.java
+++ app/src/main/java/com/boxnotfound/sinewavegenerator/model/PitchRepository.java
@@ -1,4 +1,66 @@
 package com.boxnotfound.sinewavegenerator.model;
 
+import android.app.Application;
+import android.arch.lifecycle.LiveData;
+import android.os.AsyncTask;
+
+import com.boxnotfound.sinewavegenerator.model.room.AppDatabase;
+
+import java.util.List;
+
 public class PitchRepository {
+
+    private PitchDao pitchDao;
+    private LiveData<List<Pitch>> allPitches;
+
+    public PitchRepository(Application application) {
+        AppDatabase db = AppDatabase.getDatabase(application);
+        pitchDao = db.pitchDao();
+        allPitches = pitchDao.getAllPitches();
+    }
+
+    public LiveData<List<Pitch>> getAllPitches() {
+        return allPitches;
+    }
+
+    public void insert(final Pitch pitch) {
+        new insertPitchAsyncTask(pitchDao).execute(pitch);
+    }
+
+    private static class insertPitchAsyncTask extends AsyncTask<Pitch, Void, Void> {
+        PitchDao asyncPitchDao;
+
+        insertPitchAsyncTask(PitchDao dao) {
+            asyncPitchDao = dao;
+        }
+
+        @Override
+        protected Void doInBackground(Pitch... params) {
+            asyncPitchDao.insert(params[0]);
+            return null;
+        }
+    }
+
+    public LiveData<Pitch> getPitch(String pitchName) {
+        return pitchDao.getPitch(pitchName);
+    }
+
+    public void deleteAllPitches() {
+        new deleteAllPitchesAsyncTask(pitchDao).execute();
+    }
+
+    private static class deleteAllPitchesAsyncTask extends AsyncTask<Void, Void, Void> {
+        PitchDao asyncPitchDao;
+
+        deleteAllPitchesAsyncTask(PitchDao dao) {
+            asyncPitchDao = dao;
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            asyncPitchDao.deleteAllPitches();
+            return null;
+        }
+    }
+
 }
