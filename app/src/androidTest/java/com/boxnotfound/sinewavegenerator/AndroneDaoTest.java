821effc8342249d923d03f3337fa74f3b8a9c87c
Kevin Mark Price <KPrice725@aol.com>
2019-04-01 15:01:28 -0400

    test: Add additional tests for AndroneDao
    insertAndroneReplacesOnConflict() - ensure inserted Androne
    sharing an ID with an existing Androne replaces the existing
    Androne.
    
    insertAndroneAndGetList() - insert a new Androne and
    ensure call for List<Androne> returns a list containing
    that Androne.
    
    updateAndroneAndGetById() - insert androne, update the
    existing object, update the dao, get the androne back
    from the Dao and ensure it contains the updated values.
    
    All tests pass to completion as of the time of this commit

--- app/src/androidTest/java/com/boxnotfound/sinewavegenerator/AndroneDaoTest.java
+++ app/src/androidTest/java/com/boxnotfound/sinewavegenerator/AndroneDaoTest.java
@@ -1,6 +1,10 @@
 package com.boxnotfound.sinewavegenerator;
 
+import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.MediatorLiveData;
+import android.arch.lifecycle.Observer;
 import android.arch.persistence.room.Room;
+import android.support.annotation.Nullable;
 
 import com.boxnotfound.sinewavegenerator.constants.Waveform;
 import com.boxnotfound.sinewavegenerator.model.Androne;
@@ -12,6 +16,8 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.List;
+
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -20,9 +26,11 @@ import androidx.test.core.app.ApplicationProvider;
 
 public class AndroneDaoTest {
 
+    private MediatorLiveData<List<Androne>> mAndroneLive;
     private AppDatabase db;
     private AndroneDao dao;
     private Androne androne;
+    private static final int ID = 1337;
 
     @Before
     public void setup() {
@@ -30,12 +38,15 @@ public class AndroneDaoTest {
         dao = db.androneDao();
         Pitch.instantiatePitchNames(ApplicationProvider.getApplicationContext());
         androne = new Androne(new Pitch(440.0), Waveform.SINE);
+        androne.setId(ID);
+        mAndroneLive = new MediatorLiveData<>();
     }
 
     @After
     public void cleanup() {
         db.close();
         Pitch.releasePitchNames();
+        mAndroneLive = null;
     }
 
     @Test
@@ -50,6 +61,49 @@ public class AndroneDaoTest {
         Androne loaded = dao.getAndroneById(androne.getId());
 
         assertAndrone(loaded, androne.getId(), androne.getFrequency(), androne.getPitchName(), androne.getCents(), androne.getWaveform());
+    }
+
+    @Test
+    public void insertAndroneReplacesOnConflict() {
+        dao.insert(androne);
+
+        Androne newAndrone = new Androne(new Pitch(400.0), Waveform.SAWTOOTH);
+        newAndrone.setId(ID);
+
+        dao.insert(newAndrone);
+
+        Androne loadedAndrone = dao.getAndroneById(ID);
+
+        assertAndrone(loadedAndrone, androne.getId(), newAndrone.getFrequency(), newAndrone.getPitchName(), newAndrone.getCents(), newAndrone.getWaveform());
+    }
+
+    @Test
+    public void insertAndroneAndGetList() {
+        dao.insert(androne);
+
+        List<Androne> list = dao.getAllAndrones();
+
+        assertThat(list, notNullValue());
+
+        assertThat(list.size(), is(1));
+
+        assertAndrone(list.get(0), androne.getId(), androne.getFrequency(), androne.getPitchName(), androne.getCents(), androne.getWaveform());
+    }
+
+    @Test
+    public void updateAndroneAndGetById() {
+        dao.insert(androne);
+
+        Androne updatedAndrone = new Androne(new Pitch(500.0), Waveform.SQUARE);
+        updatedAndrone.setId(ID);
+
+        dao.updateAndrone(updatedAndrone);
+
+        Androne loadedAndrone = dao.getAndroneById(ID);
+
+        assertAndrone(loadedAndrone, updatedAndrone.getId(), updatedAndrone.getFrequency(), updatedAndrone.getPitchName(), updatedAndrone.getCents(), updatedAndrone.getWaveform());
+
+
     }
 
     private void assertAndrone(Androne loadedAndrone, int id, double frequency, String pitchName, String cents, Waveform waveform) {
