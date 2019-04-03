52e08f94af80508cb3093ecbb1df57c93e8cc843
Kevin Mark Price <KPrice725@aol.com>
2019-04-03 10:54:20 -0400

    feat: Build out AndronePresenter's functions
    Implement presenter's overridden functions to
    properly handle AndroneActivity events

--- app/src/main/java/com/boxnotfound/sinewavegenerator/andrones/AndronePresenter.java
+++ app/src/main/java/com/boxnotfound/sinewavegenerator/andrones/AndronePresenter.java
@@ -13,9 +13,9 @@ import static com.google.common.base.Preconditions.checkNotNull;
 
 public class AndronePresenter implements AndroneContract.Presenter {
 
-    private final AndroneRepository androneRepository;
+    private AndroneRepository androneRepository;
 
-    private final AndroneContract.View androneView;
+    private AndroneContract.View androneView;
 
     public AndronePresenter(@NonNull AndroneRepository androneRepository, @NonNull AndroneContract.View androneView) {
         this.androneRepository = checkNotNull(androneRepository, "androneRepo cannot be null!");
@@ -31,81 +31,115 @@ public class AndronePresenter implements AndroneContract.Presenter {
 
     @Override
     public void loadAndrones() {
+        androneRepository.getAllAndrones(new AndroneDataSource.LoadAndronesCallback() {
+            @Override
+            public void onAndronesLoaded(List<Androne> andrones) {
+                if (andrones != null && !andrones.isEmpty()) {
+                    androneView.displayAndrones(andrones);
+                } else {
+                    androneView.displayNoAndrones();
+                }
+            }
 
+            @Override
+            public void onAndronesNotAvailable() {
+                androneView.displayNoAndrones();
+            }
+        });
     }
 
     @Override
     public void addAndrone(Androne androne) {
-
+        androneRepository.addAndrone(androne);
+        androneView.displayAddAndrone();
     }
 
     @Override
     public void setPitch(Androne androne, double frequency) {
-
+        androne.setFrequency(frequency);
+        updateAndrone(androne);
     }
 
     @Override
     public void setPitch(Androne androne, String pitchName) {
-
+        androne.setPitchName(pitchName);
+        updateAndrone(androne);
     }
 
     @Override
     public void incrementFrequency(Androne androne) {
-
+        androne.incrementFrequency();
+        updateAndrone(androne);
     }
 
     @Override
     public void decrementFrequency(Androne androne) {
-
+        androne.decrementFrequency();
+        updateAndrone(androne);
     }
 
     @Override
     public void incrementPitch(Androne androne) {
-
+        androne.incrementPitch();
+        updateAndrone(androne);
     }
 
     @Override
     public void decrementPitch(Androne androne) {
-
+        androne.decrementPitch();
+        updateAndrone(androne);
     }
 
     @Override
     public void setWaveform(Androne androne, Waveform waveform) {
-
+        androne.setWaveform(waveform);
+        updateAndrone(androne);
     }
 
     @Override
     public void setVolume(Androne androne, float volume) {
-
+        androne.setVolume(volume);
+        updateAndrone(androne);
     }
 
     @Override
     public void setVolume(Androne androne, int volumeProgress) {
-
+        androne.setVolumeProgress(volumeProgress);
+        updateAndrone(androne);
     }
 
     @Override
     public void playAndrone(Androne androne) {
-
+        androne.startAndrone();
+        updateAndrone(androne);
     }
 
     @Override
     public void stopAndrone(Androne androne) {
-
+        androne.stopAndrone();
+        updateAndrone(androne);
     }
 
     @Override
     public void deleteAndrone(Androne androne) {
-
+        androneRepository.deleteAndrone(androne);
+        androneView.displayDeleteAndrone();
     }
 
     @Override
     public void deleteAllAndrones() {
-
+        androneRepository.deleteAllAndrones();
+        androneView.displayNoAndrones();
     }
 
     @Override
     public void onDestroy() {
+        androneRepository = null;
+        androneView = null;
+    }
 
+    private void updateAndrone(Androne androne) {
+        androneRepository.updateAndrone(androne);
+        androneView.displayUpdateAndrone();
     }
 }
